1047b9615fd85b273a36c29778f8ca877a7bdbdf
Adam Vlček <79417338+Wlczak@users.noreply.github.com>
2024-03-15 08:21:46 +0000

    SQL update
    Co-authored-by: Rehakj <Rehakj@users.noreply.github.com>

---
+++ src/classes.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("vendor/autoload.php");
+$latte = new Latte\Engine;
+
+$latte->setTempDirectory('temp');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "travelblog";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SET CHARACTER SET UTF8";
+$conn->query($sql);
+
+$sql = "SELECT Title FROM articles";
+
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    $i = 0;
+    var_dump($result);
+    while ($row = $result->fetch_assoc()) {
+
+        $title[$i];
+        echo "<td>" . $row["Title"] . "</td>";
+    }
+}
+$conn->close();
