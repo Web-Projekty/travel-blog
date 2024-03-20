7279e4ab85418dc21106bdf5c5fdd59c0dd735f5
Wlczak <adivlcak@seznam.cz>
2024-03-20 15:47:03 +0100

    Backend baseline for #1 completed
    Co-Authored-By: Rehakj <132693892+Rehakj@users.noreply.github.com>

--- src/articleDetail.php
+++ src/articleDetail.php
@@ -1,5 +1,5 @@
 <?php
-
+############### autoload ###############
 require_once "../vendor/autoload.php";
 $latte = new Latte\Engine;
 
@@ -7,6 +7,7 @@ $latte->setTempDirectory('temp');
 
 include("../config/mysql.php");
 
+############### sql connection ###############
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
@@ -16,16 +17,44 @@ $sql = "SELECT * FROM Articles WHERE idArticles = $articleId";
 
 $result = $conn->query($sql);
 
+############### sql data extraction ###############
 if ($result->num_rows > 0) {
-
     while ($row = $result->fetch_assoc()) {
-        $title = $row['Title'];
-        $content = $row['Content'];
+        $title = $row['title'];
+        $content = $row['content'];
+        $img = $row['profileImg'];
+        $author = $row['author'];
+        $destination = $row['destination'];
+        $date = $row['datePublic'];
     }
+
+    ########### foreign key get data ###########
+    # for destination
+    $sql = "SELECT name FROM Destinations WHERE idDestination = $destination";
+    $result = $conn->query($sql);
+    $destination = $result->fetch_row()[0];
+    # for user
+    $sql = "SELECT user FROM Users WHERE idUsers = $author";
+    $result = $conn->query($sql);
+    $user = $result->fetch_row()[0];
+    $succesfull = true;
+} else {
+    $succesfull = false;
+    $errorMsg = "Database request failed.";
 }
-$content = explode("|",$content);
+
 $conn->close();
+if ($succesfull) {
+    $params = [
+        'title' => $title,
+        'content' => $content,
+        'img' => $img,
+        'author' => $user,
+        'destination' => $destination,
+        'date' => $date
+    ];
 
-$params = ['title' => $title,'content'=>$content];
-
-$latte->render('../templates/articleDetail.latte', $params);
+    $latte->render('../templates/articleDetail.latte', $params);
+} else {
+    echo $errorMsg;
+}
