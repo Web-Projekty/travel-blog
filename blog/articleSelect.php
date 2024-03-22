d454d299e2a1dff8b8ad8e21f2d33e61232adea4
Wlczak <adivlcak@seznam.cz>
2024-03-22 18:39:35 +0100

    Moved code from artSel to artClass

--- blog/articleSelect.php
+++ blog/articleSelect.php
@@ -1,43 +1,19 @@
 <?php
-
+############### autoload ###############
 require_once "../vendor/autoload.php";
 $latte = new Latte\Engine;
 
 $latte->setTempDirectory('../temp');
 
-class articleSelect
-{
-    public $title;
+############### main php import ###############
 
-    function getTitle()
-    {
-        include("../config/mysql.php");
-        $conn = new mysqli($servername, $username, $password, $dbname);
-        if ($conn->connect_error) {
-            die("Connection failed: " . $conn->connect_error);
-        }
-        $sql = "SELECT Title FROM Articles";
+include("../src/articleClasses.php");
 
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            // output data of each row
-
-            $i = 0;
-            while ($row = $result->fetch_assoc()) {
-                $title[$i] = $row['Title'];
-                $i++;
-            }
-            return $title;
-            $conn->close();
-        }
-    }
-}
-$acrticleSelect = new articleSelect();
+$Articles = new Articles();
 
 
 $params = [
-    'title' => $acrticleSelect->getTitle()
+    'title' => $Articles->getTitleArray()
 ];
 
 
--- src/articleClasses.php
+++ src/articleClasses.php
@@ -1,1 +1,29 @@
 <?php
+class Articles
+{
+    public $title;
+    ############### returns array of all article titles ###############
+    function getTitleArray()
+    {
+        include("../config/mysql.php");
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $sql = "SELECT Title FROM Articles";
+
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            // output data of each row
+
+            $i = 0;
+            while ($row = $result->fetch_assoc()) {
+                $title[$i] = $row['Title'];
+                $i++;
+            }
+            return $title;
+            $conn->close();
+        }
+    }
+}
