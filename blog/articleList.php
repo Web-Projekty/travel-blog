51cfd59da9c87ae2468f8d5b87f4a22af9daeb04
Rehakj <janrehakof@seznam.cz>
2024-04-12 16:10:15 +0200

    added form
    výpis {$searchInput} nefunguje a chat GPT si myslí že: "it indicates that the variable is not being properly parsed by the Latte template engine" takže to tady zatím naházím, ať to tu alespoň je

--- blog/articleList.php
+++ blog/articleList.php
@@ -5,12 +5,21 @@ $latte = new Latte\Engine;
 
 include "../src/listClasses.php";
 $latte->setTempDirectory('../temp');
+
 $Lists = new ArticleList();
 $lists = $Lists->getArticleList($_GET['sql']);
+
+$searchInput = "";
+if(isset($_GET["searchInput"])){
+    $searchInput = $_GET["searchInput"];
+}
+$orderBy = "";
+if(isset($_GET["orderBy"])){
+    $orderBy = $_GET["orderBy"];
+}
+
 $params = [
     'lists' => $lists
 ];
-
-
 
 $latte->render('../templates/articleList.latte', $params);
