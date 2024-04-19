d15df633c1a6b63f2c206313de213691d87833a6
florixak <florixakcz@seznam.cz>
2024-04-19 08:26:03 +0200

    test

---
+++ src/scripts/script.js
@@ -0,0 +1,36 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+console.log("Im working");
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+function nextSlide() {
+  plusSlides(1);
+}
