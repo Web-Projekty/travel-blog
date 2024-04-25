730da7f89f55be151062c76a57d70eb3d0c34807
Wlczak <adivlcak@seznam.cz>
2024-04-25 22:10:21 +0200

    Resolved #24

--- index.php
+++ index.php
@@ -3,5 +3,9 @@ require_once "vendor/autoload.php";
 $latte = new Latte\Engine;
 $latte->setTempDirectory('temp');
 
+if(isset($_GET['searchInput'])){
+    header("location: blog/articleSearch?searchInput");
+}
+
 $params = [];
 $latte->render('templates/homePage.latte', $params);
