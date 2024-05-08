70c6c9eba5ce5b38cae33a6e601074c7f7d19478
Adam Vlček <adivlcak@seznam.cz>
2024-05-08 11:01:45 +0200

    Quick fix

--- account/login.php
+++ account/login.php
@@ -5,6 +5,8 @@ $latte = new Latte\Engine;
 $latte->setTempDirectory('../temp');
 
 $Auth = new Auth;
+$login['status'] = false;
+$login['msg'] = null;
 
 if (!empty($_POST['username']) && !empty($_POST['password'])) {
     $login = $Auth->login($_POST['username'], $_POST['password']);
