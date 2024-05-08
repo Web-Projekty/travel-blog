1ad5c2649870dae610c77dd2be2b92d02c9540d4
Adam Vlček <adivlcak@seznam.cz>
2024-05-08 16:21:13 +0200

    Better readability

--- tests/testAutorun.php
+++ tests/testAutorun.php
@@ -4,6 +4,7 @@ $phpFiles = glob("*.php");
 ### executes all files except this one ###
 foreach ($phpFiles as $file) {
     if ($file !== basename(__FILE__)) {
+        echo $file."\n";
         require_once $file;
     }
 }
