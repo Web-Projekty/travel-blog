feda917003c55b6819c074507e2a326ac631a132
Adam Vlček <adivlcak@seznam.cz>
2024-05-11 15:04:49 +0200

    Parallel testing fix

--- tests/userAuthClassesTest.php
+++ tests/userAuthClassesTest.php
@@ -2,6 +2,7 @@
 ############### autoload ###############
 
 require_once "../vendor/autoload.php";
+
 use Tester\Assert;
 
 class AuthTest extends Tester\TestCase
@@ -54,7 +55,7 @@ class AuthTest extends Tester\TestCase
             ["Testing User 5", "@testingmail.test", "forTestingPurposes", "forTestingPurposes", false],
             ["Testing User 6", "testuser6testingmail.test", "forTestingPurposes", "forTestingPurposes", false],
             ["Testing User 7", "testuser7@testingst", "forTestingPurposes", "forTestingPurposes", false],
-            ["Testing User 8", "testuser8@tes#&@|€[|€^˘[Đ]][Đ][tiail.test", "forTestingPurposes", "forTestingPurposes", false], 
+            ["Testing User 8", "testuser8@tes#&@|€[|€^˘[Đ]][Đ][tiail.test", "forTestingPurposes", "forTestingPurposes", false]
         ];
     }
     /**
@@ -62,6 +63,12 @@ class AuthTest extends Tester\TestCase
      */
     public function testRegister($name, $email, $password, $Cpassword, $exitCode)
     {
+        if ($this->Database->rowExists("Users", "userEmail", $email)) {
+            sleep(15);
+        }
+        if ($this->Database->rowExists("Users", "userEmail", "existujicimail@existujicimail.mail")) {
+            sleep(15);
+        }
         // vytvoření účtu pro simulaci existujícího účtu
         $result = $this->Auth->register("heslo", "heslo", "Účet pro simulaci existujícího účtu", "existujicimail@existujicimail.mail");
 
