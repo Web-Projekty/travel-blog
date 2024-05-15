f8699a181f66c229b65e6ddf77e51672db02202a
Adam Vlček <adivlcak@seznam.cz>
2024-05-15 10:36:38 +0200

    logout working

--- account/logout.php
+++ account/logout.php
@@ -1,3 +1,15 @@
+<?php
+############### autoload ###############
+require_once "../vendor/autoload.php";
+$latte = new Latte\Engine;
+$latte->setTempDirectory('../temp');
+
+$Auth = new Auth;
+
+if ($Auth->getAuthDetail()[0]) {
+    $Auth->logout();
+}
+?>
 <p>Byli jste úspěšně odhlášeni</p>
 <script>
     setTimeout(function() {
