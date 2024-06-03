11b63b6e4e92c84f16f8b0bc97642b1d06764fff
Adam Vlček <adivlcak@seznam.cz>
2024-06-04 00:32:17 +0200

    Brocken version

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -8,12 +8,40 @@ function plusSlides(n) {
 function currentSlide(n) {
     showSlides((slideIndex = n));
 }
-
+var video = document.getElementById("video");
+var slide = document.getElementById("slide");
+var fade = document.getElementsByClassName("fade");
+var dots = document.getElementsByClassName("dot");
 function showSlides(n) {
+    console.log(n);
     var i;
-    var video = document.getElementById("video");
-    var slide = document.getElementById("slide");
-    var dots = document.getElementsByClassName("dot");
+
+    //create
+    var newVideo = document.createElement("video");
+    var newSource = document.createElement("source");
+    //add id
+    newVideo.id = "loadingVideo";
+    newSource.id = "loadingSlide";
+    //set src
+    newSource.src = "src/res/vid/v" + n + ".mp4";
+    newSource.type = "video/mp4";
+
+    //set other atributes
+    newVideo.setAttribute("autoplay", "");
+    newVideo.setAttribute("muted", "");
+    newVideo.setAttribute("loop", "");
+    newVideo.setAttribute("onended", "nextSlide()");
+
+    //append
+    fade[0].appendChild(newVideo);
+    loadingVideo = document.getElementById("loadingVideo");
+    loadingVideo.appendChild(newSource);
+    loadingSlide = document.getElementById("loadingSlide");
+
+    //old
+    //video.style.opacity = "0";
+    //slide.src = "src/res/vid/v" + n + ".mp4";
+
     /*if (n > slides.length) {
         slideIndex = 1;
     }
@@ -28,8 +56,28 @@ function showSlides(n) {
     }
     slides[slideIndex - 1].style.display = "block";
     dots[slideIndex - 1].className += " active";*/
-    slide.src = "src/res/vid/v" + n + ".mp4";
-    video.load();
+
+    //event listeners
+    loadingVideo.addEventListener("loadeddata", changeSlides());
+}
+function changeSlides() {
+    console.log("loaded");
+
+    //remove old video
+    if (slide != null) {
+        slide.remove;
+    }
+    if (video != null) {
+        video.remove;
+    }
+
+    //rename ids
+    loadingSlide.id = "slide";
+    loadingVideo.id = "video";
+    loadingVideo.load();
+
+    loadingVideo.style.opacity = "1";
+    loadingVideo.removeEventListener("loadeddata",changeSlides())
 }
 
 function nextSlide() {
