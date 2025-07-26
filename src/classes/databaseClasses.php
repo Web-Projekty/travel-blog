c709a7d6c065a6f74d391a8130f6b7b580035b1d
Adam Vlček <vlcek-adam@seznam.cz>
2025-07-26 17:55:53 +0200

    cwd?????

--- src/classes/databaseClasses.php
+++ src/classes/databaseClasses.php
@@ -28,7 +28,7 @@ class Database
                     throw new Exception("Connection failed: " . $conn->connect_error);
                 }
         }catch (Exception $e) {
-            var_dump(exec('pwd'));
+            echo getcwd();
             if (!file_exists('../db/test.db')) {
                 $conn = new PDO('sqlite:../db/test.db');
                 $sql = file_get_contents('../sql/TravelBlog.sql');
