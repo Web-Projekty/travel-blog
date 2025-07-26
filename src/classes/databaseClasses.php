e5522b51ca6ed8f17a1fed409146849e097c4af6
Adam Vlček <vlcek-adam@seznam.cz>
2025-07-26 18:11:26 +0200

    Update databaseClasses.php

--- src/classes/databaseClasses.php
+++ src/classes/databaseClasses.php
@@ -67,12 +67,9 @@ class Database
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             }
+            $result = $conn->query($sql);
+            $conn->close();
         }
-
-       
-
-        $result = $conn->query($sql);
-        $conn->close();
         return $result;
     }
     public function rowExists(string $db, string $column, string $row)
